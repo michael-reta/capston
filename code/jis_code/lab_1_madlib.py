2837e58cb22e9795bd53ee6052e0095c644ae997
jis1720 <jis1720@yahoo.com>
2022-03-29 19:03:54 -0700

    Fixed file with repetion

--- code/jis_code/lab_1_madlib.py
+++ code/jis_code/lab_1_madlib.py
@@ -1,6 +1,6 @@
 import random
 
-x = input("Would you like to try agai? Entuer yes/no: ")
+x = input("Would you like to play the madlib? yes/no: ")
 
 
 while x != 'no':
@@ -18,7 +18,10 @@ while x != 'no':
     list_d = d.split()
     d = random.choice(list_d)
 
-
-
-    print(f"My name is {a_1} {a} and on {c} I will recieve the best news ever from {b}. {b} will offer me a position as a {d} and it weill be a remote postion in {e}! Aren't you glad you did this madlib?" )
     
+
+
+
+    print(f"My name is {a_1} {a} and on {c} I will recieve the best news ever from {b}. \n{b} will offer me a position as a {d} and it weill be a remote postion in {e}! Aren't you glad you did this madlib?" )
+    
+    x = input("Would you like to try again? Entuer yes/no: ")
