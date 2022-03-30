b0397362be0e4f9c7ee225fdbd524a1e011188ba
TSCSuser <info@tscs.tech>
2022-03-29 21:48:31 -0700

    Py Challenge files

---
+++ code/gustavo/array.py
@@ -0,0 +1,22 @@
+tc1 = []
+tc2 = []
+tc3 = []
+test_condition1 = 'I love arrays they are my favorite'
+test_condition2 = 'I love days off'
+test_condition3 = 'my favorite season is'
+
+tc1 = test_condition1.split()
+tc2 = test_condition2.split()
+tc3 = test_condition3.split()
+
+
+def div_array():
+    tc1 = test_condition1.split()
+    return 
+    
+print(div_array)
+
+
+
+
+
