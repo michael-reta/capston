ddfa5ae7a6ea9c4fc2de999f97068d066603224c
deikner3 <69986521+deikner3@users.noreply.github.com>
2022-03-30 00:19:05 -0400

    split_strings.py

---
+++ code/darrell/split_strings.py
@@ -0,0 +1,11 @@
+test_condition1 = 'I love arrays they are my favorite'
+test_condition2 = 'I love days off'
+test_condition3 = 'my favorite season is'
+
+list1 = test_condition1.split()
+list2 = test_condition2.split()
+list3 = test_condition3.split()
+
+print(list1)
+print(list2)
+print(list3)
