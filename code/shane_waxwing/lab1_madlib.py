b42a5b8a7775fa33923166fd9190050e121a9039
ShaneWaxwing <shane.d.waxwing@gmail.com>
2022-03-29 22:46:39 -0400

    Added lab1 madlib

---
+++ code/shane_waxwing/lab1_madlib.py
@@ -0,0 +1,30 @@
+# Original song: Flow by Shawn James
+
+earth = input('Enter a noun: ')
+round = input('Enter an adjective: ')
+world = input('Enter a noun: ')
+change = input('Enter a present tense very without "-ing": ')
+seen = input('Enter a past tense verb: ')
+sun = input('Enter a noun: ')
+greet = input('Enter a verb: ')
+dawn = input('Enter a noun: ')
+earth_2 =input('Enter a noun: ')
+feet = input('Enter another noun: ')
+storm = input('Enter yet another noun: ')
+lost = input('Please enter a past tense verb: ')
+won = input('Enter yet another past tense verb: ')
+flowed = input('Enter one more past tense verb: ')
+river = input('Last but not least, enter a noun: ')
+
+
+
+print("\nI woke up to find the " + earth + " was " + round + " one day")
+print("And all around the " + world + ", now, things began to " + change +".")
+print("Never had I ever " + seen + " the " + sun + ",")
+print("Rise that way to " + greet + " the " + dawn + ".")
+
+print("And the " + earth_2 + " shook beneath my " + feet +".")
+print("And the " + storm + " raged through the night.")
+print("And I " + lost + " all that I had " + won + ".")
+print("Oh, but I " + flowed + " like the " + river + ".")
+
