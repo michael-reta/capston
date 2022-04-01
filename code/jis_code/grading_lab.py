fb9ca8521b50fb815e2c0ffef75a4c91223d024b
jis1720 <jis1720@yahoo.com>
2022-03-31 20:52:06 -0700

    Version 2 again

--- code/jis_code/grading_lab.py
+++ code/jis_code/grading_lab.py
@@ -4,12 +4,27 @@ grade = input("Please enter a grade: " )
 grade = float(grade)
 
 if grade >= 90:
-    print("A")
+    if grade%10 >= 5:
+        print("A+")
+    else:
+        print("A-")
 elif grade <= 89 and grade >= 80:
-    print("B")
+    if grade%10 >= 5:
+        print("B+")
+    else:
+        print("B-")
 elif grade <= 79 and grade >= 70:
-    print("C")
+    if grade%10 >= 5:
+        print("C+")
+    else:
+        print("C-")
 elif grade <= 69 and grade >=60:
-    print("D")
-else:
-    print("F")
+    if grade%10 >= 5:
+        print("D+")
+    else:
+        print("D-")
+if grade <= 59:
+    if grade%10 >= 5:
+        print("F+")
+    else:
+        print("F-")
