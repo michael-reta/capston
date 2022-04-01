7dac988dc72be1143092cb2527e3248dc59a2715
deikner3 <69986521+deikner3@users.noreply.github.com>
2022-04-01 00:12:49 -0400

    updated the conditional logic to include +s and -s

--- code/darrell/Grading.py
+++ code/darrell/Grading.py
@@ -1,14 +1,30 @@
 #ask the user for their numerical score
 score = input("Please enter your exam score: ")
-score = int(score)
-if score >= 90:
+score = float(score)
+if score >= 97.5 and score <= 100:
+    grade = "A+"
+elif score >= 92.5 and score <= 97.4:
     grade = "A"
-elif score >=80 and score <90:
+elif score >= 90 and score <= 92.4:
+    grade = "A-"
+elif score >=87.5 and score <90:
+    grade = "B+"
+elif score >= 82.5 and score <= 87.4:
     grade = "B"
-elif score >=70 and score <80:
+elif score >= 80 and score <= 82.4:
+    grade = "B-"
+elif score >=77.5 and score <80:
+    grade = "C+"
+elif score >= 72.5 and score <= 77.4:
     grade = "C"
-elif score >=60 and score <70:
+elif score >= 70 and score <= 72.4:
+    grade = "C-"
+elif score >=67.5 and score <70:
+    grade = "D+"
+elif score >= 62.5 and score <= 67.4:
     grade = "D"
+elif score >= 60 and score <= 62.4:
+    grade = "D-"
 else:
     grade = "F"
 
