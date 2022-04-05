5886a86e0f8ea1703b7f37e258febf56df41f129
ShaneWaxwing <shane.d.waxwing@gmail.com>
2022-04-05 00:28:00 -0400

    Updated grading to incluse + and -

--- code/shane_waxwing/grading.py
+++ code/shane_waxwing/grading.py
@@ -3,6 +3,7 @@
 number_grade = input('Please enter your number grade: ')
 number_grade = int(number_grade)
 
+
 if number_grade <= 59:
     letter_grade = "F"
 elif number_grade <= 69:
@@ -14,4 +15,24 @@ elif number_grade <= 89:
 else:
     letter_grade = "A"
 
-print(letter_grade)
+
+mod_number = number_grade % 10
+
+
+
+
+
+if number_grade == 100:
+    letter_grade = "A+"
+
+if number_grade >= 60 and number_grade != 100:
+    if mod_number == 0 or mod_number <= 2:
+        mod = '-'
+    elif mod_number == 3 or mod_number <= 6:
+        mod = ""
+    elif mod_number == 7 or mod_number <= 9:
+        mod = '+'
+
+    print(letter_grade + mod)
+else:
+    print(letter_grade)
