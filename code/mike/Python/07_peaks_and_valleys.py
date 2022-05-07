60b004ebda75fca4234a60533fbe9dc6c7760491
michael-reta <mreta86@gmailc.om>
2022-05-06 20:20:57 -0700

    adding some labs

---
+++ code/mike/Python/07_peaks_and_valleys.py
@@ -0,0 +1,10 @@
+# peaks(data) - Returns the indices of peaks. A peak has a lower number on both the left and the right.
+# valleys(data) - Returns the indices of 'valleys'. A valley is a number with a higher number on both the left and the right.
+# peaks_and_valleys(data) - uses the above two functions to compile a single list of the peaks and valleys in order of appearance in the original data.
+
+# data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+
+def say_hello():
+    print("hello")
+
+say_hello()
